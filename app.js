ec0a0909210541504f0147d336158b1480e96641
Kara9125 <K.nicholas9125@gmail.com>
2015-12-19 18:00:36 -0800

    basically done

--- app.js
+++ app.js
@@ -2,13 +2,6 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
 function returnAjax(){
-	// function checkRSVP(value){
-	// 	if (value === "N/A")
-	// 		return "Not necessary"
-	// 	else {
-	// 		return "Click Here to RSVP"
-	// 	}
-	// }
 
 	return $.ajax({
 			url: "http://d345h07ts0fu2m.cloudfront.net/379/data48.json",
@@ -22,23 +15,23 @@ function returnAjax(){
 
 			var englishEntries = data.categories.EnglishEvents2015.entries
 			$.each (englishEntries, function(index, value){
-				// value.
-				$("#main-content").append("<p>"+ value.month +" "+ value.day+", "+ value.time+"</p>");
-				$("#main-content").append("<p>"+ value.storename +"</p>");
-				$("#main-content").append("<input type='button' id="+index+" class='more-btn' value='See More'>");
-				
+
+				$("#main-content").append(
+					"<div class='block col-xs-12 col-sm-6 col-sm-offset-3'><p>"+ value.month +" "+ value.day+", "+ value.time+"</p><p>"+ value.storename +"</p><button id="+index+" class='more-btn btn'>See More</button></div>"
+					);
 
 			});
 
 			$(document).ready(function(){
 			  $(".more-btn").click(function(event){
 			  	var index = event.target.id;
+			    $("body").addClass("lightbox-open");
 			    $("#lightbox, #lightbox-panel").fadeIn(300);
-			    $("#lightbox-panel").append("<p>Full Event Info</p>");
-			    $("#lightbox-panel").append("<p>"+ englishEntries[index].month+", "+ englishEntries[index].day+"</p>");
-			    $("#lightbox-panel").append("<p>"+ englishEntries[index].time +"</p>");
-			    $("#lightbox-panel").append("<p>"+ englishEntries[index].storename +", "+ englishEntries[index].city+"</p>");
-			    $("#lightbox-panel").append("<p>"+ englishEntries[index].floor +"</p>");
+			    $("#lightbox-panel").append("<h1>Full Event Info</h1>");
+			    $("#lightbox-panel").append("<h3>"+ englishEntries[index].month+" "+ englishEntries[index].day+"</h3>");
+			    $("#lightbox-panel").append("<h3>"+ englishEntries[index].time +"</h3>");
+			    $("#lightbox-panel").append("<h3>"+ englishEntries[index].storename +", "+ englishEntries[index].city+"</h3>");
+			    $("#lightbox-panel").append("<h3>"+ englishEntries[index].floor +"</h3>");
 			    $("#lightbox-panel").append("<p>"+ englishEntries[index].desc +"</p>");
 
 			    if (englishEntries[index].rsvp === "N/A")
@@ -48,13 +41,13 @@ function returnAjax(){
 			    }
 
 			    
-			    $("#lightbox-panel").append("<a id='close-panel' href='#'>Close</a>");
+			    $("#lightbox-panel").append("<a id='close-panel' class='btn' href='#'>Close</a>");
 
 			    
 			    $("a#close-panel").click(function(event){
 			    	event.preventDefault();
 			    	console.log(event);
-			    	console.log("hi doggie")
+			    	$("body").removeClass("lightbox-open");
 			    $("#lightbox, #lightbox-panel").fadeOut(300);
 			    $("#lightbox-panel").empty();
 
